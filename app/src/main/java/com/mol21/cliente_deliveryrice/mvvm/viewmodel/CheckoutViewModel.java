d77b479c14cb1f8829e8842e5492ce4b6abb4785
mol211 <victormolins92@gmail.com>
2025-05-15 19:48:23 +0200

    administrador muestra pedidos en curso

--- app/src/main/java/com/mol21/cliente_deliveryrice/mvvm/viewmodel/CheckoutViewModel.java
+++ app/src/main/java/com/mol21/cliente_deliveryrice/mvvm/viewmodel/CheckoutViewModel.java
@@ -3,12 +3,15 @@ package com.mol21.cliente_deliveryrice.mvvm.viewmodel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.mol21.cliente_deliveryrice.mvvm.model.DTO.ItemDTO;
 import com.mol21.cliente_deliveryrice.mvvm.model.DTO.PedidoDTO;
 import com.mol21.cliente_deliveryrice.mvvm.model.EstadoPedido;
 import com.mol21.cliente_deliveryrice.mvvm.repository.CheckoutRepository;
 import com.mol21.cliente_deliveryrice.utils.GenericResponse;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import javax.inject.Inject;
 
@@ -18,12 +21,15 @@ import dagger.hilt.android.lifecycle.HiltViewModel;
 
 public class CheckoutViewModel extends ViewModel {
     private final CheckoutRepository CHECKOUTREPOSITORY;
+    List<ItemDTO>pedidosEnProceso = new ArrayList<>();
+    List<ItemDTO>pedidosEnviados = new ArrayList<>();
+    List<ItemDTO>pedidosPentiente = new ArrayList<>();
 
     @Inject
     public CheckoutViewModel(CheckoutRepository checkoutrepository) {
         CHECKOUTREPOSITORY = checkoutrepository;
     }
-    public LiveData<GenericResponse<List<PedidoDTO>>> obtenerPedidos(Long idUsuario, long idRepartidor, EstadoPedido estado){
+    public LiveData<GenericResponse<List<PedidoDTO>>> obtenerPedidos(Long idUsuario, Long idRepartidor, EstadoPedido estado){
 
         return this.CHECKOUTREPOSITORY.obtenerPedidos(idUsuario, idRepartidor, estado);
     }
@@ -35,4 +41,15 @@ public class CheckoutViewModel extends ViewModel {
     }
 
 
+    public List<LiveData<GenericResponse<List<PedidoDTO>>>> obtenerPedidosEnProceso() {
+        LiveData<GenericResponse<List<PedidoDTO>>> pedidosEnProceso = obtenerPedidos(null,null,EstadoPedido.EN_PROCESO);
+        LiveData<GenericResponse<List<PedidoDTO>>> pedidosEnviados = obtenerPedidos(null,null,EstadoPedido.ENVIADO);
+        LiveData<GenericResponse<List<PedidoDTO>>> pedidosPendiente = obtenerPedidos(null,null,EstadoPedido.PENDIENTE);
+        List<LiveData<GenericResponse<List<PedidoDTO>>>> listaDeListas = new ArrayList<>();
+        listaDeListas.add(pedidosPendiente);
+        listaDeListas.add(pedidosEnviados);
+        listaDeListas.add(pedidosEnProceso);
+
+        return listaDeListas;
+    }
 }
--- app/src/main/java/com/mol21/cliente_deliveryrice/ui/fragment/CurrentPedidosFragment.java
+++ app/src/main/java/com/mol21/cliente_deliveryrice/ui/fragment/CurrentPedidosFragment.java
@@ -1,26 +1,110 @@
 package com.mol21.cliente_deliveryrice.ui.fragment;
 
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.mol21.cliente_deliveryrice.databinding.FragmentCurrentPedidosBinding;
+import com.mol21.cliente_deliveryrice.mvvm.model.DTO.ItemDTO;
+import com.mol21.cliente_deliveryrice.mvvm.model.DTO.PedidoDTO;
+import com.mol21.cliente_deliveryrice.mvvm.model.EstadoPedido;
+import com.mol21.cliente_deliveryrice.mvvm.viewmodel.CheckoutViewModel;
+import com.mol21.cliente_deliveryrice.ui.adapter.PedidoAdapter;
+import com.mol21.cliente_deliveryrice.utils.GenericResponse;
+import com.mol21.cliente_deliveryrice.utils.SessionManager;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.inject.Inject;
 
 
 public class CurrentPedidosFragment extends Fragment {
 
     private FragmentCurrentPedidosBinding binding;
+    private CheckoutViewModel checkoutViewModel;
+
+    @Inject
+    SessionManager sessionManager;
+    List<PedidoDTO> listaPedidos;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentCurrentPedidosBinding.inflate(inflater, container, false);
+        initViewModel();
+        listaPedidos = new ArrayList<>();
         View root = binding.getRoot();
-
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        init();
+
+    }
+
+    private void init() {
+        List<LiveData<GenericResponse<List<PedidoDTO>>>>listaDeListas = checkoutViewModel.obtenerPedidosEnProceso();
+        for(LiveData<GenericResponse<List<PedidoDTO>>> liveData : listaDeListas) {
+            liveData.observe(getViewLifecycleOwner(),response->{
+                for(PedidoDTO pedido: listaPedidos){
+                        Log.d("Pedido","pedido:"+pedido.getId());
+                    }
+                Log.d("RESPONSE","response:"+response.getRpta());
+
+                if(response.getRpta()==1){
+                    listaPedidos.addAll(response.getBody());
+                }
+                if(listaPedidos.isEmpty()){
+                    binding.llNoPedido.setVisibility(VISIBLE);
+                    binding.ndSiPedidos.setVisibility(GONE);
+                } else {
+                    binding.llNoPedido.setVisibility(GONE);
+                    binding.ndSiPedidos.setVisibility(VISIBLE);
+                    initRecyclerView();
+                }
+            });
+        }
+    }
+
+    private void initRecyclerView() {
+        Map<EstadoPedido,Integer> ordenEstado = Map.of(
+                EstadoPedido.PENDIENTE,1,
+                EstadoPedido.EN_PROCESO,2,
+                EstadoPedido.ENVIADO,3
+        );
+        List<PedidoDTO>listaOrdenada = listaPedidos.stream()
+                        .sorted(Comparator.comparing(p-> ordenEstado.get(p.getEstadoPedido())))
+                                .collect(Collectors.toList());
+        binding.rvCurrentPedidos.setLayoutManager(new LinearLayoutManager(getContext()));
+        binding.rvCurrentPedidos.setAdapter(new PedidoAdapter(listaOrdenada, pedido->{
+            mostrarBottomSheetCambiarEstadoPedido(pedido);
+    }));
+    }
+
+    private void mostrarBottomSheetCambiarEstadoPedido(PedidoDTO pedido) {
+        BottomSheetCambiarEstadoPedido dialog = new BottomSheetCambiarEstadoPedido(pedido);
+        dialog.show(getChildFragmentManager(), "Cambiar Estado Pedido");
+    }
+
+    private void initViewModel() {
+        checkoutViewModel = new ViewModelProvider(requireActivity()).get(CheckoutViewModel.class);
     }
 
     @Override
